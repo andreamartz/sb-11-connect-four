a33e0511e0af5e2cc52841bcc2eea1dd19d8278a
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-20 12:37:07 -0500

    Style game & refactor functions

--- connect4.js
+++ connect4.js
@@ -11,12 +11,15 @@ const HEIGHT = 6;
 let currPlayer = 1; // active player: 1 or 2
 const board = []; // array of rows, each row is array of cells  (board[y][x])
 let boardIsFull = false;
+let allowTurn = true;
 
+const turn = document.querySelector("#turn");
+turn.innerText = `${currPlayer}'s`;
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
-function makeBoard() {
+const makeBoard = () => {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
   for (let i = 0; i < HEIGHT; i++) {
     // for every row of the board
@@ -28,7 +31,7 @@ function makeBoard() {
     board.push(row);
   }
   return board;
-}
+};
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
@@ -69,7 +72,7 @@ function makeHtmlBoard() {
 }
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
-function findSpotForCol(x) {
+const findSpotForCol = (x) => {
   // TODO: write the real version of this, rather than always returning 0
   for (let y = board.length - 1; y >= 0; y--) {
     if (board[y][x] === null) {
@@ -77,16 +80,20 @@ function findSpotForCol(x) {
     }
   }
   return null;
-}
+};
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
-function placeInTable(y, x) {
+const placeInTable = (y, x) => {
   // TODO: make a div and insert into correct table cell
   // make piece div
   const piece = document.createElement("div");
   piece.classList.add("piece");
   piece.classList.add("player" + currPlayer);
+  // piece.style.top = `${50 * (y + 2)}px`;
+  // piece.style.top = 0;
+  // console.log("top: ", piece.style.top);
+
   // insert piece div into correct td
 
   const targetTdId = `${y}-${x}`;
@@ -94,63 +101,61 @@ function placeInTable(y, x) {
   // const td = document.querySelector(`#${y}-${x}`);  // why doesn't this work?
   console.log(td);
   td.append(piece);
-}
+};
 
 /** endGame: announce game end */
 
-function endGame(msg) {
-  // TODO: pop up alert message
-  if (tie) {
-    alert(msg);
-  } else {
-    alert(msg);
-  }
-}
+const endGame = (msg) => {
+  // TODO: pop up alert message after small delay
+  setTimeout(() => alert(msg), 300);
+  allowTurn = false;
+};
 
 /** handleClick: handle click of column top to play piece */
 
-function handleClick(evt) {
+const handleClick = (evt) => {
   // get x from ID of clicked cell
-  const x = +evt.target.id;
+  if (allowTurn) {
+    const x = +evt.target.id;
 
-  // get next spot in column (if none, ignore click)
-  const y = findSpotForCol(x);
-  if (y === null) {
-    return;
-  }
+    // get next spot in column (if none, ignore click)
+    const y = findSpotForCol(x);
+    if (y === null) {
+      return;
+    }
 
-  // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
-  placeInTable(y, x);
-  board[`${y}`][`${x}`] = currPlayer;
+    // place piece in board and add to HTML table
+    // TODO: add line to update in-memory board
+    placeInTable(y, x);
+    board[`${y}`][`${x}`] = currPlayer;
 
-  // check for win
-  if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
-  }
+    // check for win
+    if (checkForWin(y, x)) {
+      return endGame(`Player ${currPlayer} won!`);
+    }
 
-  // check for tie
-  // TODO: check if all cells in board are filled; if so, call endGame
-  const rowFills = [];
-  for (let y = 0; y < board.length; y++) {
-    const currRowIsFull = board[y].every((el) => el !== null);
-    rowFills.push(currRowIsFull);
-  }
-  for (let x = 0; x < rowFills; x++) {
-    boardIsFull = rowFills.every((el) => el === true);
-  }
-  if (boardIsFull) {
-    endGame("Game over with no winner");
-  }
+    // check for tie
+    // TODO: check if all cells in board are filled; if so, call endGame
+    const rowFills = [];
+    for (let y = 0; y < board.length; y++) {
+      const currRowIsFull = board[y].every((el) => el !== null);
+      rowFills.push(currRowIsFull);
+    }
+    boardIsFull = rowFills.every((bool) => bool === true);
+    if (boardIsFull) {
+      return endGame("Tied Game");
+    }
 
-  // switch players
-  // TODO: switch currPlayer 1 <-> 2
-  currPlayer = currPlayer === 1 ? 2 : 1;
-}
+    // switch players
+    // TODO: switch currPlayer 1 <-> 2
+    currPlayer = currPlayer === 1 ? 2 : 1;
+    turn.innerText = `${currPlayer}'s`;
+  } else return;
+};
 
-/** checkForWin: check board cell-by-cell for "does a win start here?" */
+/** checkForWin: check last cell played: does a win start or end here? */
 
-function checkForWin() {
+function checkForWin(y, x) {
   function _win(cells) {
     // Check four cells to see if they're all color of current player
     //  - cells: list of four (y, x) cells
@@ -166,42 +171,69 @@ function checkForWin() {
     );
   }
 
-  // TODO: read and understand this code. Add comments to help you.
-
-  for (let y = 0; y < HEIGHT; y++) {
-    for (let x = 0; x < WIDTH; x++) {
-      // for every cell on the board...
-      // 1. ...generate the four possible winning cell "coordinate" combinations in the up and right direction
-      // 2. ...include coordinates of impossible cells
-      const horiz = [
-        [y, x],
-        [y, x + 1],
-        [y, x + 2],
-        [y, x + 3],
-      ];
-      const vert = [
-        [y, x],
-        [y + 1, x],
-        [y + 2, x],
-        [y + 3, x],
-      ];
-      const diagDR = [
-        [y, x],
-        [y + 1, x + 1],
-        [y + 2, x + 2],
-        [y + 3, x + 3],
-      ];
-      const diagDL = [
-        [y, x],
-        [y + 1, x - 1],
-        [y + 2, x - 2],
-        [y + 3, x - 3],
-      ];
-      // 3. ...check for a win in any of the four cell combinations
-      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-        return true;
-      }
-    }
+  // For the most recent turn taken,
+  // 1. ...generate the eight possible winning cell "coordinate" combinations
+  // 2. ...include the coordinates of impossible cells outside the board
+  const horizR = [
+    [y, x],
+    [y, x + 1],
+    [y, x + 2],
+    [y, x + 3],
+  ];
+  const horizL = [
+    [y, x],
+    [y, x - 1],
+    [y, x - 2],
+    [y, x - 3],
+  ];
+  const vertD = [
+    [y, x],
+    [y + 1, x],
+    [y + 2, x],
+    [y + 3, x],
+  ];
+  const vertU = [
+    [y, x],
+    [y - 1, x],
+    [y - 2, x],
+    [y - 3, x],
+  ];
+  const diagDR = [
+    [y, x],
+    [y + 1, x + 1],
+    [y + 2, x + 2],
+    [y + 3, x + 3],
+  ];
+  const diagUR = [
+    [y, x],
+    [y - 1, x + 1],
+    [y - 2, x + 2],
+    [y - 3, x + 3],
+  ];
+  const diagDL = [
+    [y, x],
+    [y + 1, x - 1],
+    [y + 2, x - 2],
+    [y + 3, x - 3],
+  ];
+  const diagUL = [
+    [y, x],
+    [y - 1, x - 1],
+    [y - 2, x - 2],
+    [y - 3, x - 3],
+  ];
+  // 3. ...check for a win in any of the eight cell combinations
+  if (
+    _win(horizR) ||
+    _win(horizL) ||
+    _win(vertD) ||
+    _win(vertU) ||
+    _win(diagDR) ||
+    _win(diagUR) ||
+    _win(diagDL) ||
+    _win(diagUL)
+  ) {
+    return true;
   }
 }
 
